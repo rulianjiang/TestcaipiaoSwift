d1d02401a705a0f4245eccc949249a0c3ec5ec4c
shine <b777888@188.com>
2018-08-22 00:35:56 +0800

    设置我的彩票界面普通方式

--- caipiaoSwift/Classes/Setting[设置]/Controller/ZXMSettingTableViewController.swift
+++ caipiaoSwift/Classes/Setting[设置]/Controller/ZXMSettingTableViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ZXMSettingTableViewController: UITableViewController {
+    
+    //重写init方法,在我的彩票界面不用再设置界面的布局样式.
+    init() {
+    super.init(style: .grouped)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// 可重用cell.
+    let cellID = "cellID"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //注册cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,71 +35,80 @@ class ZXMSettingTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
 
+}
+
+extension ZXMSettingTableViewController {
+    
+    // MARK: - 实现数据源方法.
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 3
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        if section == 0 {
+            return 1
+        } else {
+            return 4
+        }
+    
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        //搭建界面.
+        if indexPath.section == 0 && indexPath.row == 0 {
+            //第0组
+            cell.imageView?.image = UIImage(named: "RedeemCode")
+            cell.textLabel?.text = "使用兑换码"
+        } else  if (indexPath.section == 1) {
+            //第一组.
+            if indexPath.row == 0 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "推送提醒"
+            } else if indexPath.row == 1 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "使用摇一摇"
+            }else if indexPath.row == 2 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "声音效果"
+            } else if indexPath.row == 3 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "购彩小助手"
+            }
+            
+        } else  if (indexPath.section == 2) {
+            //第二组.
+            if indexPath.row == 0 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "检查新版本"
+            } else if indexPath.row == 1 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "分享"
+            }else if indexPath.row == 2 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "产品推荐"
+            } else if indexPath.row == 3 {
+                cell.imageView?.image = UIImage(named: "RedeemCode")
+                cell.textLabel?.text = "关于"
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
+    
+    
 }
